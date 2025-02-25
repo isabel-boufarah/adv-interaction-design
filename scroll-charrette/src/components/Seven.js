3201340ec6fc87133a4bfd059f087af970493fe6
Isabel Boufarah <bisabel@wustl.edu>
2025-02-24 21:31:08 -0600

    update scroll

--- scroll-charrette/src/components/Seven.js
+++
@@ -1,7 +0,0 @@
-import styles from '../styles/seven.module.css'
-
-export default function Seven() {
-    return (<div className={styles.main}>
-        <div className={styles.sq1}></div>
-    </div>)
-}
--- scroll-charrette/src/components/Three.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/three.module.css'
-
-export default function Three() {
-    return (
-        <div className={styles.main}>
-            <div className={styles.square}></div>
-        </div>
-    )
-}
